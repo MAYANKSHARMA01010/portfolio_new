debb06170f0e8a5526144341db29ad456dbb353f
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-19 22:52:07 +0530

    Footer added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/footer";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
