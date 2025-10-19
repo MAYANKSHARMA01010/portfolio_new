330423df56ec5e81774c60cd4050ae4daa3882b6
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-10-19 22:50:40 +0530

    nodemon and watch added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,6 @@ export default function Navbar() {
     return (
         <div className="flex items-center justify-between px-6 py-4 bg-gray-900 text-white shadow-md">
             <div>
-                <h1 className="text-3xl font-bold text-cyan-400">Mayank</h1>
-            </div>
-            <div>
                 <ul className="flex flex-wrap gap-6 text-lg font-medium">
                     <li><a href="#home" className="hover:text-cyan-400 transition">About</a></li>
                     <li><a href="#about" className="hover:text-cyan-400 transition">Projects</a></li>
